8c0476979d5beba898a37132850eb3983eeeee12
Pete Kreppein <pmkreppein@gmail.com>
2018-12-14 19:58:36 -0500

    signup working, beautificaiton turned on.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,24 @@ class UsersController < ApplicationController
     erb :'users/create_account'
   }
 
-  
+  post('/users/create_account'){
+  if params[:username] == "" || params[:email] == "" || params[:password] == "" || params[:first_name] == ""
+    redirect to '/users/create_account'
+  else
+     @user = User.new(:username => params[:username], :email => params[:email], :first_name => params[:first_name], :password => params[:password])
+     @user.save
+     session[:user_id] = @user.id
+     redirect to '/dreams/me'
+  end
+}
+
+get('/login'){
+  if !logged_in?
+    erb :'users/login'
+  else
+    redirect to '/dreams'
+  end
+}
 
 
 end #classend
