31043b9f66193cfed8ef3c59b470a1772e9ff2d7
Pete Kreppein <pmkreppein@gmail.com>
2018-12-14 22:29:44 -0500

    New and View dreams working

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,14 +1,63 @@
 class DreamsController < ApplicationController
 
-get('/dreams/me'){
-  if logged_in?
-  erb :'dreams/me'
-else
-  redirect to '/users/login'
-end
-}
 
+  get('/dreams/new') do
+    if logged_in?
+      erb :'dreams/new_dream'
+    else
+      @message = 'You must be logged in to create a dream'
+      erb :'users/login'
+  end
+  end
 
+  post('/dreams/new') do
+    @dreams = []
+    if logged_in?
+        @dream = Dream.new(:description => params[:description],
+        :image_link => params[:image_link],
+        :user_id => current_user.id
+      )
+      @dream.save
+      @dreams << @dream
+      @message = "Dream created successfully"
+      erb :'dreams/dream'
+    else
+      @message = 'You must be logged in to create a dream'
+      erb :'users/login'
+  end
+  end
 
+  get('/dreams') do
+    if logged_in?
+      @dreams = Dream.all(:user_id => current_user.id)
 
-end #classend
+      erb :'dreams/dream'
+    else
+      @message = 'You must be logged in to view dreams'
+      erb :'users/login'
+  end
+  end
+
+  get('/dreams/me') do
+    @dreams = []
+    if logged_in?
+
+      @dreams = Dream.where(:user_id => current_user.id)
+
+      @user_string = "All your Dreams, #{current_user.first_name}"
+      erb :'dreams/dream'
+    else
+      @message = 'You must be logged in to view dreams'
+      erb :'users/login'
+  end
+  end
+
+  get('/dreams/members/:username') do
+    if logged_in?
+      erb :'dreams/me'
+    else
+      @message = 'You must be logged in to create a dream'
+      erb :'users/login'
+  end
+  end
+end # classend
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,56 +1,51 @@
 class UsersController < ApplicationController
-
-  get('/users/create-account'){
+  get('/users/create-account') do
     erb :'users/create_account'
-  }
+  end
 
-  post('/users/create_account'){
-
-  if !User.find_by(:email => params[:email])
-      if params[:username] == "" || params[:email] == "" || params[:password] == "" || params[:first_name] == ""
-        @message = "Sorry, you need to fill out the form in its entirety to sign up."
+  post('/users/create_account') do
+    if !User.find_by(email: params[:email])
+      if params[:username] == '' || params[:email] == '' || params[:password] == '' || params[:first_name] == ''
+        @message = 'Sorry, you need to fill out the form in its entirety to sign up.'
         erb :'users/create_account'
-    else
-       @user = User.new(:username => params[:username], :email => params[:email], :first_name => params[:first_name], :password => params[:password])
-       @user.save
-       binding.pry
-       session[:user_id] = @user.id
-       redirect to '/dreams/me'
+      else
+        @user = User.new(username: params[:username], email: params[:email], first_name: params[:first_name], password: params[:password])
+        @user.save
+        session[:user_id] = @user.id
+        redirect to '/dreams/me'
      end
-  else
-     @message = "Looks like you already have an account with us, or another error took place."
-     erb :'users/create_account'
-  end
-}
-
-get('/users/login'){
-  if !logged_in?
-    erb :'users/login'
-  else
-    redirect to '/dreams'
-  end
-}
-
-post('/users/login'){
-
-  user = User.find_by(:email => params[:email])
-
-  if user && user.authenticate(params[:password])
-    session[:user_id] = user.id
-    redirect to '/dreams/me'
-  else
-    @message = "Sorry, either your username or password was invalid.  Try again."
-    erb :'users/login'
-  end
-}
-
-get('/logout'){
-  if logged_in?
-    session.destroy
-    redirect to '/'
-  else
-    redirect to '/'
+    else
+      @message = 'Looks like you already have an account with us, or another error took place.'
+      erb :'users/create_account'
+    end
   end
 
-}
-end #classend
+  get('/users/login') do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/dreams'
+    end
+  end
+
+  post('/users/login') do
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/dreams/me'
+    else
+      @message = 'Sorry, either your username or password was invalid.  Try again.'
+      erb :'users/login'
+    end
+  end
+
+  get('/logout') do
+    if logged_in?
+      session.destroy
+      redirect to '/'
+    else
+      redirect to '/'
+    end
+  end
+end # classend
